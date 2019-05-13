7ee57980d6c15bd98ff438dfb75d82d8a8135420
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-05-13 12:04:00 -0500

    Chapter 6

--- src/TEstScoreStatistics.java
+++ src/TEstScoreStatistics.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class TEstScoreStatistics {
     public static void main(String[] args) {
         int score;
-        String choise;
         int count = 0;
-        int avr;
         int high = 0;
         int low = 100;
-        int A = 0;
-        int B = 0;
-        int C = 0;
-        int D = 0;
-        int F = 0;
+        int ATotal = 0;
+        int BTotal = 0;
+        int CTotal = 0;
+        int DTotal = 0;
+        int FTotal = 0;
         while (count != 999) {
             Scanner k = new Scanner(System.in);
             System.out.println("enter test score ");
@@ -20,24 +18,17 @@ public class TEstScoreStatistics {
 
             if (score > high) {
                 high = score;
-            } else if (score < 100) {
+            }
+            else if (score < 100) {
                 low = score;
             }
-            else if(score > 90||score<100)
-                ++A;
-            else if(score >= 80||score <= 89)
-                ++B;
-            else if(score >= 70 || score <= 79)
-                ++C;
-            else if(score >= 60 || score <= 69)
-                ++D;
-            else if(score <= 59)
-                ++F;
-            System.out.println("A total" );displayBar(A);
-            System.out.println("B total ");displayBar(B);
-            System.out.println("C total ");displayBar(C);
-            System.out.println("D total ");displayBar(D);
-            System.out.println("F total ");displayBar(F);
+            message(score,ATotal,BTotal,CTotal,DTotal,FTotal);
+
+            System.out.println("A total " );displayBar(ATotal);
+            System.out.println("B total ");displayBar(BTotal);
+            System.out.println("C total ");displayBar(CTotal);
+            System.out.println("D total ");displayBar(DTotal);
+            System.out.println("F total ");displayBar(FTotal);
             ++count;
         }
     }
@@ -45,8 +36,24 @@ public class TEstScoreStatistics {
     public static void displayBar(int b) {
         for (int i = 0; i < b; ++i) {
             System.out.print("/");
+
         }
 
 
-    }
+        }
+
+
+
+    public static void message(int score,int ATotal,int BTotal,int CTotal, int DTotal, int FTotal) {
+    if (score > 90 || score < 100)
+        ++ATotal;
+    else if (score >= 80 || score <= 89)
+        ++BTotal;
+    else if (score >= 70 || score <= 79)
+        ++CTotal;
+    else if (score >= 60 || score <= 69)
+        ++DTotal;
+    else if (score <= 59)
+        ++FTotal;
+}
 }
